1a027cb1cdde56ac7705266d548fb219b393b07b
ossmanmejia <79607140+ossmanmejia@users.noreply.github.com>
2023-01-16 12:23:33 -0500

    Update2023

---
+++ build/precache-manifest.968933e0051fce2f20d815ede38024d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcc57635ef4c048309855a04272eb6b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a01682f7080a85ba98b3",
+    "url": "/static/js/2.f5905640.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/static/js/2.f5905640.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd8e0b37fab7a686c224",
+    "url": "/static/js/main.1ee87d92.chunk.js"
+  },
+  {
+    "revision": "0dec160f9db644705374",
+    "url": "/static/js/runtime-main.e6ed48ca.js"
+  }
+]);
